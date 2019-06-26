b8a0403b559c27838ddbabf093d280531f982b06
MilcahHalili <halilicenteno@gmail.com>
2019-06-26 11:52:38 -0700

    install express, mailchimp api, and dotenv

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d01e336a5ad6408ffcba";
+/******/ 	var hotCurrentHash = "66a025f4b4e50e219aae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 9001;
+app.listen(port);
+
+console.log(`express app listening on port ${port}`);
