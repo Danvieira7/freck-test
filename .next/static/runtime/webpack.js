598dc856c702893012db0af431f5e262ae213b74
Freck Dev <freckdev@Frecks-MacBook-Air.local>
2019-08-22 13:40:43 -0700

    save free shipping bar progress

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e90e70e26119acbce589";
+/******/ 	var hotCurrentHash = "0ddcc59a71f6d71318ac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
