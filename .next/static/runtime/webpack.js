5c60efacb5bd7bddd281b214c9e4f625cbbc37c5
Freck Dev <freckdev@Frecks-MacBook-Air.local>
2019-08-23 14:14:25 -0700

    commiting remove cart item function

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "754259dbaddcf11d28cc";
+/******/ 	var hotCurrentHash = "f76ea1aa06a125202e8f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
